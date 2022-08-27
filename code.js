01530eca6653053ca5707883e52a42088aa3a822
saanikasoni19 <88622734+saanikasoni19@users.noreply.github.com>
2022-08-27 19:21:03 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,15 @@
+function pageHTMLHtml(){
+    window.location = "../html_htmlfolder/html.html";
+}
+
+function pageCSSHtml(){
+    window.location = "../style_htmlfolder/style.html";
+}
+
+function mainPage(){
+    window.location = "../main_page/index.html";
+}
+
+function MeHTML(){
+    window.location = "../me_htmlfolder/me.html";
+}
